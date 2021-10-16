26653775b6d29c1bdf5c58cabb59253526a6f274
karnavsheth <karnavsheth@yahoo.in>
2021-10-16 17:23:44 +0100

    Strategy Pattern implemented

---
+++ Promotion_Engine/Promotion_Business_Logic/IPromotionStrategy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promotion_Engine.Promotion_Business_Logic
+{
+    public interface IPromotionStrategy
+    {
+        string Name { get; }
+        int GetPriceAfterPromotionalDiscount(int[] num);
+    }
+}
---
+++ Promotion_Engine/Promotion_Business_Logic/Type1PromotionStrategy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promotion_Engine.Promotion_Business_Logic
+{
+    public class Type1PromotionStrategy : IPromotionStrategy
+    {
+        public string Name => throw new NotImplementedException();
+
+        public int GetPriceAfterPromotionalDiscount(int[] num)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
